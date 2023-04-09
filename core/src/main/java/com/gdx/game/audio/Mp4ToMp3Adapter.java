50eb40bf270efff1fbe36e22acbc4dcaf55e7771
khaledmahdi7 <khaled.mahdi@etu.univ-tours.fr>
2023-04-09 21:34:16 +0200

    Modification finale des classes audio de ryadh

--- core/src/main/java/com/gdx/game/audio/Mp4ToMp3Adapter.java
+++ core/src/main/java/com/gdx/game/audio/Mp4ToMp3Adapter.java
@@ -1,38 +1,32 @@
 package com.gdx.game.audio;
 
-import com.badlogic.gdx.audio.Music;
+import com.gdx.game.audio.AudioManager;
+import com.gdx.game.audio.AudioObserver;
+import com.gdx.game.audio.AudioSubject;
 
-public class Mp4ToMp3Adapter extends AudioManager {
+public class Mp4ToMp3Adapter extends AudioManager implements AudioObserver {
 
     private Mp4Player mp4Player;
 
-    public Mp4ToMp3Adapter() {
+    public Mp4ToMp3Adapter(AudioSubject audioSubject) {
         super();
+        audioSubject.addObserver(this);
         mp4Player = new Mp4Player();
     }
 
     @Override
     public void onNotify(AudioCommand command, AudioTypeEvent event) {
-        switch (command) {
-            case MUSIC_LOAD:
-                mp4Player.loadMp4(event.getValue());
-                break;
-            case MUSIC_PLAY_ONCE:
-                Music music = mp4Player.playMp4(event.getValue(), false);
-                setCurrentMusic(music);
-                break;
-            case MUSIC_PLAY_LOOP:
-                Music musicLoop = mp4Player.playMp4(event.getValue(), true);
-                setCurrentMusic(musicLoop);
-                break;
-            case MUSIC_STOP:
-                mp4Player.stopMp4();
-                break;
-            case MUSIC_STOP_ALL:
-                mp4Player.stopAllMp4();
-                break;
-            default:
-                break;
+        if (command == AudioCommand.MUSIC_PLAY_ONCE || command == AudioCommand.MUSIC_PLAY_LOOP) {
+            String mp4FilePath = event.getValue().replace(".mp3", ".mp4");
+            String mp3FilePath = event.getValue().replace(".mp4", "_converted.mp3");
+
+            // Convertir le fichier MP4 en MP3
+            Mp4ToMp3Converter.convertMp4ToMp3(mp4FilePath, mp3FilePath);
+
+            // Lire le fichier MP3 converti avec AudioManager
+            super.onNotify(command, AudioTypeEvent.NONE);
+        } else {
+            super.onNotify(command, event);
         }
     }
 }
